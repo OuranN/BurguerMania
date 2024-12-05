7df0c536a72e1b297da7a6597bc48914bdb6625d
Ouran <cristhiandev20@gmail.com>
2024-12-04 21:03:10 -0300

    update

--- src/app/views/detail-page/detail-page.component.ts
+++ src/app/views/detail-page/detail-page.component.ts
@@ -5,7 +5,6 @@ import { Button1Component } from "../../button1/button1.component";
 import { DetailsComponent } from "../../details/details.component";
 import { CommonModule } from '@angular/common';
 import { CardDetailsComponent } from "../../card-details/card-details.component";
-import { ProductService } from '../../services/product/product.service';
 
 @Component({
   selector: 'app-detail-page',
@@ -16,30 +15,74 @@ import { ProductService } from '../../services/product/product.service';
 })
 export class DetailPageComponent implements OnInit {
 
-  burguers: Burguer[] = [];
+  burguers: Burguer[] = [
+    { 
+      name: 'X-Alface-Premium', 
+      description: 'Pão, Habúrguer, alface, tomate, queijo e maionese', 
+      price: 35.00, imageUrl: '/images/hamburguer.png', 
+      category: 'X-Vegan',
+      completeDescription:'Um hambúrguer vegano suculento feito com uma base de grão-de-bico e quinoa, temperado com especiarias defumadas, cebola caramelizada e alho, garantindo uma textura rica e saborosa. Servido em um pão macio, ele vem acompanhado de fatias frescas de tomate, alface crocante, picles, abacate cremoso e uma generosa camada de maionese de ervas vegana. Finalizado com molho barbecue agridoce e uma pitada de pimenta-do-reino moída na hora, proporcionando uma combinação deliciosa de sabores e texturas em cada mordida.'
+    },
+    { name: 'X-Tomate', 
+      description: 'Pão, Habúrguer, alface, tomate, queijo e maionese', 
+      price: 35.00, 
+      imageUrl: '/images/hamburguer.png', 
+      category: 'X-Vegan',
+      completeDescription:'Um hambúrguer vegano suculento feito com uma base de grão-de-bico e quinoa, temperado com especiarias defumadas, cebola caramelizada e alho, garantindo uma textura rica e saborosa. Servido em um pão macio, ele vem acompanhado de fatias frescas de tomate, alface crocante, picles, abacate cremoso e uma generosa camada de maionese de ervas vegana. Finalizado com molho barbecue agridoce e uma pitada de pimenta-do-reino moída na hora, proporcionando uma combinação deliciosa de sabores e texturas em cada mordida.'
+
+    },
+    { name: 'X-Frutas', 
+      description: 'Pão, Habúrguer, alface, tomate, queijo e maionese', 
+      price: 35.00, 
+      imageUrl: '/images/hamburguer.png', 
+      category: 'X-Vegan',
+      completeDescription:'Um hambúrguer vegano suculento feito com uma base de grão-de-bico e quinoa, temperado com especiarias defumadas, cebola caramelizada e alho, garantindo uma textura rica e saborosa. Servido em um pão macio, ele vem acompanhado de fatias frescas de tomate, alface crocante, picles, abacate cremoso e uma generosa camada de maionese de ervas vegana. Finalizado com molho barbecue agridoce e uma pitada de pimenta-do-reino moída na hora, proporcionando uma combinação deliciosa de sabores e texturas em cada mordida.'
+    },
+    { name: 'X-ExtraVegan', 
+      description: 'Pão, Habúrguer, alface, tomate, queijo e maionese', 
+      price: 0.00, 
+      imageUrl: '/images/hamburguer.png', 
+      category: 'X-Vegan',
+      completeDescription:'Um hambúrguer vegano suculento feito com uma base de grão-de-bico e quinoa, temperado com especiarias defumadas, cebola caramelizada e alho, garantindo uma textura rica e saborosa. Servido em um pão macio, ele vem acompanhado de fatias frescas de tomate, alface crocante, picles, abacate cremoso e uma generosa camada de maionese de ervas vegana. Finalizado com molho barbecue agridoce e uma pitada de pimenta-do-reino moída na hora, proporcionando uma combinação deliciosa de sabores e texturas em cada mordida.'
+    },
+    { name: 'X-Dieta', 
+      description: 'Pão, Habúrguer, alface, tomate, queijo e maionese', 
+      price: 35.00, 
+      imageUrl: '/images/hamburguer.png', 
+      category: 'X-Fitnnes',
+      completeDescription:'Um hambúrguer vegano suculento feito com uma base de grão-de-bico e quinoa, temperado com especiarias defumadas, cebola caramelizada e alho, garantindo uma textura rica e saborosa. Servido em um pão macio, ele vem acompanhado de fatias frescas de tomate, alface crocante, picles, abacate cremoso e uma generosa camada de maionese de ervas vegana. Finalizado com molho barbecue agridoce e uma pitada de pimenta-do-reino moída na hora, proporcionando uma combinação deliciosa de sabores e texturas em cada mordida.'
+    },
+    { name: 'X-Bacon', 
+      description: 'Pão, Habúrguer, bacon, queijo e maionese', 
+      price: 40.00, 
+      imageUrl: '/images/hamburguer.png', 
+      category: 'X-Infarto',
+      completeDescription:'Um hambúrguer vegano suculento feito com uma base de grão-de-bico e quinoa, temperado com especiarias defumadas, cebola caramelizada e alho, garantindo uma textura rica e saborosa. Servido em um pão macio, ele vem acompanhado de fatias frescas de tomate, alface crocante, picles, abacate cremoso e uma generosa camada de maionese de ervas vegana. Finalizado com molho barbecue agridoce e uma pitada de pimenta-do-reino moída na hora, proporcionando uma combinação deliciosa de sabores e texturas em cada mordida.'
+    },
+    { name: 'X-Picanha', 
+      description: 'Pão, Habúrguer de picanha, alface, queijo e maionese', 
+      price: 45.00, 
+      imageUrl: '/images/hamburguer.png', 
+      category: 'X-Infarto',
+      completeDescription:'Um hambúrguer vegano suculento feito com uma base de grão-de-bico e quinoa, temperado com especiarias defumadas, cebola caramelizada e alho, garantindo uma textura rica e saborosa. Servido em um pão macio, ele vem acompanhado de fatias frescas de tomate, alface crocante, picles, abacate cremoso e uma generosa camada de maionese de ervas vegana. Finalizado com molho barbecue agridoce e uma pitada de pimenta-do-reino moída na hora, proporcionando uma combinação deliciosa de sabores e texturas em cada mordida.'
+    },
+  ];
 
   selectedBurger!: Burguer;
   router: any;
 
-  constructor(private route: ActivatedRoute, private productService: ProductService) {}
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    const burgerName = this.route.snapshot.paramMap.get('name');
-    
-    this.productService.getAllProducts().subscribe(
-      (burguers) => {
-        console.log('Produtos recebidos:', burguers);
-        this.burguers = burguers;
 
-        if (burgerName) {
-          this.selectedBurger = this.burguers.find(burger => burger.name === burgerName)!;
-          console.log(this.selectedBurger);
-        } else {
-          console.error('Burger não encontrado');
-        }
-      },
-      (error) => console.error('Erro ao carregar produtos:', error)
-    );
+    const burgerName = this.route.snapshot.paramMap.get('name');
+
+    if (burgerName) {
+
+      this.selectedBurger = this.burguers.find(burger => burger.name === burgerName)!;
+      console.log(this.selectedBurger); 
+    } else {
+      console.error('Burger não encontrado');
+    }
   }
 }
-
